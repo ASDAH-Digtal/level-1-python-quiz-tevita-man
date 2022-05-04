2b26d525178881c3490654cd982b6e65d928b0d1
tevita-man <tevita_man@student.asdah.school.nz>
2022-05-04 13:24:40 +1200

    edited the print statement introducing the quiz.

--- main/quiz.py
+++ main/quiz.py
@@ -1,4 +1,5 @@
 name = input("What is you name?")
-print("Hello {} this is a quiz! This quiz is a quiz on Naruto.".format(name))
+print("Hello {}, this is a quiz! This quiz is a quiz on Naruto.".format(name))
 print("There is one rule when it comes to this quiz. Rule 1: When answering, spell the answer as it is presented. So if the answer you want to put is cat and you put kat, it will be wrong. Lets begin")
+
 
