33597334e566748ac970bad66046bedc5ec5e37c
tevita-man <tevita_man@student.asdah.school.nz>
2022-05-04 13:29:05 +1200

    added comments to help when coding.

--- main/quiz.py
+++ main/quiz.py
@@ -1,5 +1,10 @@
+#introduce the quiz.
 name = input("What is you name?")
 print("Hello {}, this is a quiz! This quiz is a quiz on Naruto.".format(name))
 print("There is one rule when it comes to this quiz. Rule 1: When answering, spell the answer as it is presented. So if the answer you want to put is cat and you put kat, it will be wrong. Lets begin")
 
+#start asking the questions of the quiz.
 
+
+
+#infrom the partipant of their score and thank them for playing.
