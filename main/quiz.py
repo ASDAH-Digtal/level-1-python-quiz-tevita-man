a03d038ad2eb542513024bbc64ffb9913e4929fc
tevita-man <tevita_man@student.asdah.school.nz>
2022-05-09 11:30:53 +1200

    i coded the first question

--- main/quiz.py
+++ main/quiz.py
@@ -5,17 +5,19 @@ print("There is one rule when it comes to this quiz. When answering, spell the a
 
 total_score = 0
 #start asking the questions of the quiz.
-q1 = input("1. ")
+q1 = input("1. Question 1: Where was Naruto Uzumaki born? \n1. The Land of Lightning/Kumogakure \n2. The Land of Water/Kirigakure \n3. The Land of Wind/Sunagakure \n4. The Land of Fire/Konohagakure. ")
 
-if q1 == "" or q1 == "":
+if q1 == "The Land of Fire." or q1 == "Konohagakure." or q1 == "4":
     print("Correct, you earned 10 points!")
     total_score += 10
-elif q1 == "" or "":
+elif q1 == "The Land of Lightning" or q1 == "Kumogakure" or q1 == "1":
     print("Sorry, that's incorrect, you lost 5 points")
-elif q1 == "" or "":
+elif q1 == "The Land of Water" or q1 == "Kirigakure" or q1 == "2":
     print("Sorry, that's incorrect, you lost 5 points")
-elif q1 == "" or "":
+elif q1 == "The Land of Wind" or q1 == "Sunagakure" or q1 == "3":
     print("Sorry, that's incorrect, you lost 5 points")
 else:
     print("That is not an option")
+
+
 #infrom the partipant of their score and thank them for playing.
