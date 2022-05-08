78565e2cca9f2dbc81306c9d1a4045445bf0d85c
tevita-man <tevita_man@student.asdah.school.nz>
2022-05-09 11:08:24 +1200

    i added the code for the questions and added the variable total_score

--- main/quiz.py
+++ main/quiz.py
@@ -3,8 +3,19 @@ name = input("What is you name?")
 print("Hello {}, this is a quiz! This quiz is a quiz on the anime, Naruto!".format(name))
 print("There is one rule when it comes to this quiz. When answering, spell the answer as it is presented. So if the answer you want to put is cat and you put kat, it will be wrong. Lets begin")
 
+total_score = 0
 #start asking the questions of the quiz.
+q1 = input("1. ")
 
-
-
+if q1 == "" or q1 == "":
+    print("Correct, you earned 10 points!")
+    total_score += 10
+elif q1 == "" or "":
+    print("Sorry, that's incorrect, you lost 5 points")
+elif q1 == "" or "":
+    print("Sorry, that's incorrect, you lost 5 points")
+elif q1 == "" or "":
+    print("Sorry, that's incorrect, you lost 5 points")
+else:
+    print("That is not an option")
 #infrom the partipant of their score and thank them for playing.
