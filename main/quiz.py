07df0954610b0ed4c3bcf44693cb565a0dd493d2
tevita-man <tevita_man@student.asdah.school.nz>
2022-05-09 14:01:47 +1200

    edited some print statements and the first question.

--- main/quiz.py
+++ main/quiz.py
@@ -1,23 +1,29 @@
 #introduce the quiz.
 name = input("What is you name?")
 print("Hello {}, this is a quiz! This quiz is a quiz on the anime, Naruto!".format(name))
-print("There is one rule when it comes to this quiz. When answering, spell the answer as it is presented. So if the answer you want to put is cat and you put kat, it will be wrong. Lets begin")
+print("Before we being, lets go over a few housekeeping rules if you will. \n Number 1: When asnwering a question, answer with either the option or option number. \n Number 2: Dont cheat, that ruins the quiz. \n That is all, Lets begin")
 
 total_score = 0
 #start asking the questions of the quiz.
-q1 = input("1. Question 1: Where was Naruto Uzumaki born? \n1. The Land of Lightning/Kumogakure \n2. The Land of Water/Kirigakure \n3. The Land of Wind/Sunagakure \n4. The Land of Fire/Konohagakure. ")
+q1 = input("Question 1: Where was Naruto Uzumaki born? \n1. The Land of Lightning/Kumogakure \n2. The Land of Water/Kirigakure \n3. The Land of Wind/Sunagakure \n4. The Land of Fire/Konohagakure. ")
 
 if q1 == "The Land of Fire." or q1 == "Konohagakure." or q1 == "4":
     print("Correct, you earned 10 points!")
     total_score += 10
 elif q1 == "The Land of Lightning" or q1 == "Kumogakure" or q1 == "1":
     print("Sorry, that's incorrect, you lost 5 points")
+    total_score -= 5
 elif q1 == "The Land of Water" or q1 == "Kirigakure" or q1 == "2":
     print("Sorry, that's incorrect, you lost 5 points")
+    total_score -= 5
 elif q1 == "The Land of Wind" or q1 == "Sunagakure" or q1 == "3":
     print("Sorry, that's incorrect, you lost 5 points")
+    total_score -= 5
 else:
     print("That is not an option")
+    total_score -= 5
 
-
+print("You current score is {}.".format(total_score))
+q2 = input
 #infrom the partipant of their score and thank them for playing.
+print("You scored {}.".format(total_score))
